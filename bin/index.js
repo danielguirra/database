021a42de408987e7044892aefe1e99cec00ff451
Daniel <daniel.guirra777@gmail.com>
2022-08-26 01:21:24 -0300

    init

--- bin/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const DataBaseConec_1 = require("./querys/DataBaseConec");
-const opa = new DataBaseConec_1.DataBaseConec().conector();
-console.log(opa);
--- bin/util/CreateTableBase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const DatabaseCreateTable_1 = require("../querys/DatabaseCreateTable");
-const create = new DatabaseCreateTable_1.DatabaseCreateTable();
-create.createTable({
-    name: "TableName",
-    columns: [],
-});
